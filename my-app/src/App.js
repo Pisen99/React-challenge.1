bde77dd75d8587398feee6deead82f5fa1d3855a
Pisen99 <patricia.andersson99@hotmail.com>
2023-02-16 23:43:03 +0000

    Challange1: part 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
-import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+import css from "./App.module.css";
+import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className={css.App}>
-      {
-        <Sidebar />
-        }
+      {}
+      <NavBarSimple />
+      <Sidebar />
     </div>
   );
 }
