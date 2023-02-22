204e6fc2f9df6920a1e5dbeb35c08c0061daaf1c
Pisen99 <patricia.andersson99@hotmail.com>
2023-02-22 12:17:47 +0000

    Part 2

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import css from "./css/Content.module.css";
 import { savedPosts } from "../posts.json";
+import PostItem from './PostItem';
 
 export class Content extends Component {
     constructor(props) {
@@ -13,7 +14,7 @@ export class Content extends Component {
                     <h1>My Posts</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    savedPosts.map()
+                <PostItem savedPosts={savedPosts} />
                 </div>
             </div>
         )
